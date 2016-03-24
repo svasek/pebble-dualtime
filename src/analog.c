57a4c45712ec46f8d050716b64898577f6285e6e
Svasek <svasek@gmail.com>
2016-03-24 17:05:53 +0100

    Added fonts

--- src/analog.c
+++ src/analog.c
@@ -3,7 +3,7 @@
 
 // BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
-static GFont s_res_gothic_24_bold;
+static GFont s_res_lcd_phone_font_26;
 static Layer *s_layer_1;
 static TextLayer *s_textlayer_1;
 
@@ -14,16 +14,16 @@ static void initialise_ui(void) {
     window_set_fullscreen(s_window, 1);
   #endif
   
-  s_res_gothic_24_bold = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
+  s_res_lcd_phone_font_26 = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_LCD_PHONE_FONT_26));
   // s_layer_1
   s_layer_1 = layer_create(GRect(-1, 0, 144, 139));
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_layer_1);
   
   // s_textlayer_1
   s_textlayer_1 = text_layer_create(GRect(21, 143, 100, 42));
-  text_layer_set_text(s_textlayer_1, "Text layer");
+  text_layer_set_text(s_textlayer_1, "23:47");
   text_layer_set_text_alignment(s_textlayer_1, GTextAlignmentCenter);
-  text_layer_set_font(s_textlayer_1, s_res_gothic_24_bold);
+  text_layer_set_font(s_textlayer_1, s_res_lcd_phone_font_26);
   layer_add_child(window_get_root_layer(s_window), (Layer *)s_textlayer_1);
 }
 
@@ -31,6 +31,7 @@ static void destroy_ui(void) {
   window_destroy(s_window);
   layer_destroy(s_layer_1);
   text_layer_destroy(s_textlayer_1);
+  fonts_unload_custom_font(s_res_lcd_phone_font_26);
 }
 // END AUTO-GENERATED UI CODE
 
